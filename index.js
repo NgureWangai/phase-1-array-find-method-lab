c39c5f5c5dbccdefa51111a613a66e1f8e6af36b
NgureWangai <ngurewangai@gmail.com>
2024-03-31 14:29:52 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record){
+    let winRecord = record.find(element => element.result === 'W')
+    if (winRecord) {
+        return winRecord.year;
+    }
+    else {
+        return undefined;
+    }
+}
+const theWinningYear = superbowlWin(record)
+console.log(theWinningYear)
